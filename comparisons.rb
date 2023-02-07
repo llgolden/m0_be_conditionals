bcb576ea3b3b31b5954a40a0226b395dd17900f2
Lauren Golden <llgolden@gmail.com>
2023-02-07 09:41:07 -0700

    Complete Part 3 and complete the Comparisons work

--- comparisons.rb
+++ comparisons.rb
@@ -74,7 +74,7 @@ puts friends > siblings
 attendees = 9
 meals = 8
 puts attendees != meals
-# YOU DO: Check if the number of attendees does not equal the number of meals
+# YOU DO: Check if the number of attendees does not equal the number of meals; true
 
 
 #-------------------
@@ -97,15 +97,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age <= 1
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: final line is true because the dog does love to play and is also a puppy as defined by being 1 year old or less
+
